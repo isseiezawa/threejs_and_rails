e83f4f266350cd1ffc7467e186484c6e79bd4262
issei_ezawa <issemann@yahoo.co.jp>
2022-09-22 22:52:01 +0900

    Add: canvasのsetup

--- app/javascript/controllers/threejs_controller.js
+++ app/javascript/controllers/threejs_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="threejs"
 export default class extends Controller {
   connect() {
+    console.log('hello', this.element)
   }
 }
