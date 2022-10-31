6836ac130f4b1a8451f052064bd3665d9f0a6ac2
issei_ezawa <issemann@yahoo.co.jp>
2022-11-01 04:27:04 +0900

    Add: GLTFLoaderを使用し、アニメーションの入った猫配置

--- app/javascript/controllers/ants_threejs_controller.js
+++ app/javascript/controllers/ants_threejs_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import * as THREE from "three"
 import { MTLLoader } from "three/MTLLoader"
 import { OBJLoader } from "three/OBJLoader"
+import { GLTFLoader } from "three/GLTFLoader"
 // 一人称視点
 import { PointerLockControls } from "../plugins/PointerLockControlsMobile"
 // 3D文字盤
@@ -94,8 +95,6 @@ export default class extends Controller {
     this.camera.position.y = 0.3
     // レンダラー作成
     this.renderer = new THREE.WebGLRenderer()
-    // 空間の色設定
-    this.renderer.setClearColor(0x7fbfff, 1.0);
     // 寸法
     this.renderer.setSize(window.innerWidth, window.innerHeight)
     // DOM要素を追加する
@@ -129,6 +128,43 @@ export default class extends Controller {
     const rockMaterial = '/assets/obj/rock/rock.mtl'
     const rockObject = '/assets/obj/rock/rock.obj'
     this.createObject(rockTexture, rockMaterial, rockObject, 40)
+
+    // 猫追加
+    const gltfLoader = new GLTFLoader()
+    this.cat = await gltfLoader.loadAsync('/assets/obj/cat/cat.gltf')
+    this.scene.add(this.cat.scene)
+    this.cat.scene.position.set(13, 0, -13)
+
+    this.cat.scene.rotation.y = 1.2 * Math.PI
+
+    // 取得したモデルのサイズを均一にするための計算
+    const box3 = new THREE.Box3()
+    // 世界軸に沿った最小のバウンディング ボックスを計算
+    box3.setFromObject( this.cat.scene )
+    // 現物のサイズを出力
+    const width = box3.max.x - box3.min.x
+    const hight = box3.max.y - box3.min.y
+    const length = box3.max.z - box3.min.z
+
+    // 最大値を取得
+    const maxSize = Math.max(width, hight, length)
+    const scaleFactor =  45 / maxSize
+
+    this.cat.scene.scale.set(scaleFactor, scaleFactor, scaleFactor)
+
+    this.cat.scene.traverse((child) => {
+      if(child.isMesh) {
+        child.material.color.r = 10
+        child.material.color.g = 10
+        child.material.color.b = 10
+      }
+    })
+
+    this.mixer = new THREE.AnimationMixer(this.cat.scene)
+
+    this.cat.animations.forEach((clip) => {
+      this.mixer.clipAction(clip).play()
+    })
 
     // 地面作成
     const groundTexture = new THREE.TextureLoader().load('/assets/obj/ground/ground.jpg')
@@ -544,7 +580,7 @@ export default class extends Controller {
     // 前後左右判定。Number(true) => 1, Number(false) => 0になる
     this.direction.z = Number(this.moveForward) - Number(this.moveBackward)
     this.direction.x = Number(this.moveRight) - Number(this.moveLeft)
-
+  
     // ポインタがONになったら(画面の中に吸い込まれたら)
     if(this.controls.isLocked) {
       // 現在のtimeと以前のprevTimeの差を取ってあげるとパソコンのフレームレートを獲得できる
@@ -639,10 +675,11 @@ export default class extends Controller {
           document.getElementById('userName').innerHTML = hitObjs[0].object.userData.name + 'に餌を与えました'
         }
       }
+
+      this.mixer.update(delta)
+
+      ThreeMeshUI.update();
     }
-
-    ThreeMeshUI.update();
-
     this.renderer.render(this.scene, this.camera)
   }
 
